21b5a29703f0348c478b5a69a6e3692195f54a2f
Bao Chi Pham <pomasurimobile@gmail.com>
2016-06-17 17:46:11 +0700

    dit me

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]){
     QApplication app(argc,argv);
     QPushButton NutBam("Dit me may con cho !");
     NutBam.show();
-    QPushButton NutBam2("Địt mẹ !");
+    QPushButton NutBam2("Địt mẹ con hchs !");
     NutBam2.show();
     return app.exec();
 }
