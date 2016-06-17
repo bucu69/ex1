18606496dc4d925d3c7ff9edf1749363e42fc274
bucu69 <pomasurimobile@gmail.com>
2016-06-17 09:02:42 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <QApplication>
+#include <QPushButton>
+int main(int argc, char *argv[]){
+    QApplication app(argc,argv);
+    QPushButton NutBam("Dit me may con cho !");
+    NutBam.show();
+    return app.exec();
+}
